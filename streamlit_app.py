aac1c04a07d811a2540760bad0d5f3cb9cce80f1
ManalHanif <manalhanif24@gmail.com>
2024-10-26 13:46:50 +0300

    Manal

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,5 @@
 import streamlit as st
-from streamlit_lottie import st_lottie
-import json
-import requests
 st.write('hello world')
 
-def load_lottieurl(url:str):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-lot_hello=load_lottieurl('https://lottie.host/5264c145-8e04-4020-a4e2-12b8cfefaf8f/naH2hYZEX1.json')
-st_lottie(lot_hello,key='Hello')
+
     
