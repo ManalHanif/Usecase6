e914a5efc61246de2a2d2b821ba85802377a99a8
ManalHanif <manalhanif24@gmail.com>
2024-10-26 22:13:29 +0300

    m

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-import pandas as pd
+
 
 
 # Center the title using Markdown
 st.markdown("<h1 style='text-align: center;'>بين الحلم و الاستثمار </h1>", unsafe_allow_html=True)
 
-df=pd.DataFrame({'col1':[1,2,3,4],'col2':[1,2,3,4]})
+df=pandas.DataFrame({'col1':[1,2,3,4],'col2':[1,2,3,4]})
 
 st.dataframe(df)
