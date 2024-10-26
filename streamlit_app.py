3bee4d6592bcfc98b1fc5df9678d50a7823a6059
ManalHanif <manalhanif24@gmail.com>
2024-10-26 21:45:22 +0300

    manal

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,8 @@
 import json
 import requests
 import streamlit as st
-from streamlit_lottie import st_lottie
+# from streamlit_lottie import st_lottie
 
 # Center the title using Markdown
 st.markdown("<h1 style='text-align: center;'>بين الحلم و الاستثمار </h1>", unsafe_allow_html=True)
 
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
-lottie_family = load_lottieurl("https://lottie.host/c0cbe4ae-6193-4417-b105-5c6519235745/hW7UA2JbjT.json")
-
-
-st_lottie(
-lottie_family,
-)
