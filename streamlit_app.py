ca91258ba00c086470ad8ba81241e0131b430f27
ManalHanif <manalhanif24@gmail.com>
2024-10-26 21:47:25 +0300

    m

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import json
 import requests
 import streamlit as st
-# from streamlit_lottie import st_lottie
+
+
 
 # Center the title using Markdown
 st.markdown("<h1 style='text-align: center;'>بين الحلم و الاستثمار </h1>", unsafe_allow_html=True)
