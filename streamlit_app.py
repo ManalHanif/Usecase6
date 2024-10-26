3efcdaaad5487ed817f5f552056408f1b45b6659
ManalHanif <manalhanif24@gmail.com>
2024-10-26 21:12:42 +0300

    m

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from PIL import Image
 image = Image.open('personn.jpg')
-st.image(image)
+st.title('بيت المتقبل')
+st.image(image,width=80,height=80)
 st.write('hello world')
 
 
