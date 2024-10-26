dd314e6144e02f2d1fa534466df2df64405312c5
ManalHanif <manalhanif24@gmail.com>
2024-10-26 21:54:56 +0300

    manal

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ import pandas as pd
 # Center the title using Markdown
 st.markdown("<h1 style='text-align: center;'>بين الحلم و الاستثمار </h1>", unsafe_allow_html=True)
 
-df=pd.read_csv('data/Riyadh_Aqqar/Aqqar_lands.csv')
+df=pd.read_csv('https://github.com/ManalHanif/Usecase6/data/Riyadh_Aqqar/Aqqar_lands.csv')
 
 df
