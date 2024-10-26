b18417edf05e1e3f21930fb7f71039af7461fc8c
ManalHanif <manalhanif24@gmail.com>
2024-10-26 23:10:47 +0300

    m

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,13 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
 
 
-df_land = pd.read_csv('Aqqar_lands.csv')
 
-neighborhood_counts = df_land['الحي'].value_counts()
 
-neighborhoods_over_5 = neighborhood_counts[neighborhood_counts > 5].index
-filtered_df = df_land[df_land['الحي'].isin(neighborhoods_over_5)]
+# Center the title using Markdown
+st.markdown("<h1 style='text-align: center;'>بيت المتقبل</h1>", unsafe_allow_html=True)
 
-avg_price_per_neighborhood = filtered_df.groupby('الحي')['السعر الإجمالي'].mean().reset_index()
-sorted_avg_prices = avg_price_per_neighborhood.sort_values(by='السعر الإجمالي', ascending=False)
-# Select the top 5 and bottom 5 neighborhoods by average price
-top_5 = sorted_avg_prices.head(5)
-bottom_5 = sorted_avg_prices.tail(5)
-# Combine top 5 and bottom 5 into a single DataFrame for plotting
-top_bottom_5 = pd.concat([top_5, bottom_5])
+st.markdown("<h4 style='text-align: center;'>كان أبو خالد رجلاً طموحاً، يعيش حياة مليئة بالتحديات والتطلعات الكبيرة. لديه ثلاثة أطفال يعشقهم، وكان دائماً يطمح لتوفير أفضل حياة لهم. في أحد الأيام، جاءه قرار مفاجئ؛ الانتقال إلى مدينة الرياض، المدينة الكبيرة التي تحفل بالفرص. لم يكن الانتقال سهلاً، لكنه كان يرى في الرياض بوابة لمستقبل مشرق له ولعائلته./h4>", unsafe_allow_html=True)
 
-# Create a bar chart
-fig = px.bar(
-    top_bottom_5,
-    x='الحي',
-    y='السعر الإجمالي',
-    title="أقل وأعلى 5 أحياء بناء على متوسط أسعار الأراضي",
-    labels={'الحي': 'الحي', 'السعر الإجمالي': 'متوسط السعر'},
-)
 
-# Show the chart
-fig.show()
+
+
