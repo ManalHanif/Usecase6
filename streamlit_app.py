06b7adddd27a73ab2873815d9d0a6dbba609f13b
ManalHanif <manalhanif24@gmail.com>
2024-10-26 21:10:03 +0300

    m

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from PIL import Image
-image = Image.open('https://github.com/ManalHanif/Usecase6/personn.jpg')
+image = Image.open('personn.jpg')
 st.image(image)
 st.write('hello world')
 
