0173516cb6e05bed640ebe92341411a41eb51926
ManalHanif <manalhanif24@gmail.com>
2024-10-27 00:45:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,5 @@
 import streamlit as st
 from PIL import Image
-import base64
-
-main_bg = "avg_lands.jpg"
-main_bg_ext = "jpg"
-
-side_bg = "with_aparts.jpg"
-side_bg_ext = "jpg"
-
-st.markdown(
-    f"""
-    <style>
-    .reportview-container {{
-        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
-    }}
-   .sidebar .sidebar-content {{
-        background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
 
 
