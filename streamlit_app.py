ac4c3ad4c4037551ac6cb6db6aaca95aad916aad
ManalHanif <manalhanif24@gmail.com>
2024-10-26 21:23:56 +0300

    manal

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,19 @@
 import streamlit as st
 from PIL import Image
+
+# Load the image
 image = Image.open('personn.jpg')
-st.title('بيت المتقبل')
-st.image(image,width=300)
-st.write('hello world')
+
+# Center the title using Markdown
+st.markdown("<h1 style='text-align: center;'>بين الحلم و الاستثمار </h1>", unsafe_allow_html=True)
+
+# Center the image
+st.image(image, width=200)
+
+# Display a message centered
+st.markdown("<h2 style='text-align: center;'>hello world</h2>", unsafe_allow_html=True)
+
+
 
 
     
