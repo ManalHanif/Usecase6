4c78bd9b2ec2d528d7b3be4ef01141e31a14df8f
Abdulrahman-w <144175534+Abdulrahman-w@users.noreply.github.com>
2024-10-27 07:14:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,11 +39,7 @@ lot_cod1 = load_lot("https://lottie.host/04339cb6-0438-4aa6-a4e7-c37a49e040dc/VC
 # Load animation image(2)
 
 # --  asset url: https://lottie.host/96099f2a-5cda-4f0d-82df-e8ad2fb2fcda/aKOav2Rv6Y.json
-def load_lot(url):
-    r= requests.get(url)
-    if r.status_code !=200:
-        return None
-    return r.json()
+
  
 lot_cod2 = load_lot("https://lottie.host/96099f2a-5cda-4f0d-82df-e8ad2fb2fcda/aKOav2Rv6Y.json")    
 
